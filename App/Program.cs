62c1945f94427a64d014b7bb7ca53d50b0fc52b5
Seyedamirhossein Hesamian <amirhesamyan@gmail.com>
2022-11-03 22:57:47 -0500

    started the repo

---
+++ App/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+var script = @$"
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+Install-PackageProvider -Name NuGet -Force
+Install-Module PowerShellGet -AllowClobber -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+Install-PackageProvider -Name NuGet
+
+Install-Module -Name Nuget -Scope CurrentUser -Repository PSGallery -AllowClobber
+Install-Module -Name PowerShellGet -Scope CurrentUser -Repository PSGallery -AllowClobber
+
+Install-Module -Name 'Az' -Scope CurrentUser -Repository PSGallery -Force
+
+Import-Module 'Az'
+Import-Module 'Az.Accounts'
+Import-Module 'Az.RecoveryServices'
+
+New-AzResourceGroup -Name 'TestRg123' -Location 'eastus2euap'
+";
+
+
+var initialState = InitialSessionState.CreateDefault2();
+initialState.LanguageMode = PSLanguageMode.ConstrainedLanguage;
+
+using var powershell = PowerShell.Create(initialState);
+var results = powershell
+    .AddScript(script).Invoke();
+
+foreach (var outputItem in results)
+{
+    Debug.WriteLine(outputItem);
+}
